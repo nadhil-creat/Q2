02f1a361480f7ad8f1eda0ee36b780f652eae08f
mohamed nadhil <sajidnadhil10@gmail.com>
2023-02-19 12:06:30 +0530

    no1 answer

---
+++ dbo.Courseinfo.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Courseinfo] (
+    [CourseId]      INT           NOT NULL,
+    [Name]          VARCHAR (200) NULL,
+    [Lecturer_Name] VARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([CourseId] ASC) 
+);
+
---
+++ dbo.Studentsinfo.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Studentsinfo] (
+    [StidentId] INT           NOT NULL IDENTITY,
+    [Nmae]      VARCHAR (255) NULL,
+    [City]      VARCHAR (30)  NULL,
+    [CourseId]  INT           NULL,
+    PRIMARY KEY CLUSTERED ([StidentId] ASC)
+);
+
