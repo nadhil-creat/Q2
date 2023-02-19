5a0b8bd34f4f15b6e1ead91539817d3903f27861
mohamed nadhil <sajidnadhil10@gmail.com>
2023-02-19 13:49:15 +0530

    no3 answer

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿insert into Studentsinfo values
+(1,'Kasun Gamege','Kandy',2),
+(2,'Danial Sam','Jaffna',3),
+(3,'Hansi Silva','Colombo',1),
+(4,'Ranindu Heath','Matara',3),
+(5,'Praneet Wijesingh','Kandy',4
---
+++ SQLQuery4.sql
@@ -0,0 +1,5 @@
+insert into Courseinfo values
+(1, N'Web Development', N'M.M.Herath'),
+(2, N'Graphic Design', N'j.S.V.Piyesena'),
+(3, N'Mobile Ap Development', N'K.K.S.Dias'),
+(4, N'Java', N'U.H.S.Perera');
