3838ba89b8310745264367a4c639236836ded5a6
mohamed nadhil <sajidnadhil10@gmail.com>
2023-02-19 13:07:41 +0530

    no1answer dbo.Studentsinfo3.sql

---
+++ dbo.Studentsinfo3.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Studentsinfo] (
+    [StidentId] INT           NOT NULL IDENTITY,
+    [Nmae]      VARCHAR (255) NULL,
+    [City]      VARCHAR (30)  NULL,
+    [CourseId]  INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([StidentId] ASC), 
+    CONSTRAINT [FK_Studentsinfo_Courseinfo] FOREIGN KEY ([CourseId]) REFERENCES [Studentsinfo]([CourseId]) 
+);
+
